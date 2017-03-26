23f817a4a35a63d740c9282100e37ea25cda463d
Aleksey Ushakov <leks4leks@gmail.com>
2017-03-26 16:43:45 +0600

    4task
    4task done

---
+++ 1stTask/4task/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4task.Startup))]
+namespace _4task
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ 1stTask/4task/obj/Debug/AspnetCompileMerge/Source/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_4task.MvcApplication" Language="C#" %>
